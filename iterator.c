0f28d8a5ec7b091b87a1fd1675c9043b8a200e0d
Pagwin <Pagwin.Fedora@protonmail.com>
2020-05-09 23:30:12 -0400

    map functions are complete

--- iterator.c
+++ iterator.c
@@ -13,12 +13,27 @@ iterator iter(size_t dataTypeSize,size_t listLen,void* list){
 	return return_val;
 }
 iterator iter_from_func(size_t dataTypeSize,void* (*func)(void)){
-	
+	void* currentVal = func();
+	iterator returnVal;
+	iterator* currentIter = &returnVal;
+	while(currentVal){
+		currentIter->data = currentVal;
+		currentIter->dataSize = dataTypeSize;
+		currentVal = func();
+		if(currentVal){
+			iterator next;
+			currentIter->next = &next;
+			currentIter = currentIter->next;
+		}
+	}
+	currentIter.next = NULL;
+	return returnVal;
 }
 #ifdef pagwinSpecialStringLib
 iterator iter_from_string(string str){
-	return iter(sizeof(char),str.len,str);
+	return iter(sizeof(char),str.len,str.raw);
 }
+#endif
 //returns a copy of the iterator
 iterator copy_iter(iterator vals){
 	iterator current;
@@ -32,9 +47,13 @@ iterator copy_iter(iterator vals){
 }
 void map(size_t (*func)(void*),iterator* vals){
 	while(vals){
-		vals->dataSize = 	
+		vals->dataSize = func(vals->data);
+		vals=vals->next;
 	}
 }
-void mapKnow(size_t (*func)(void*,size_t)){
-	
+void mapKnow(size_t (*func)(void*,size_t),iterator* vals){
+	while(vals){
+		vals->dataSize = =func(vals->data,vals->dataSize);
+		vals=vals->next;
+	}
 }
