c37d422ba9f23edefbe0619a5a50cdcfb4382465
Pagwin <Pagwin.Fedora@protonmail.com>
2020-09-10 23:45:34 -0400

    file cleaned up and took care of what seemed to be a memory leak

--- HMAC.c
+++ HMAC.c
@@ -1,55 +1,84 @@
 //this file is a mess and can 100% be improved on for readability
+void* (*allocator)(size_t) = malloc;
+void (*intern_free)(void*) = free;
+void setAlloc(void* (*alloc)(size_t)){
+    allocator = alloc;
+}
+void setDealloc(void (*intern_free)(void*) dealloc){
+    intern_free = dealloc;
+}
 char* HMAC(void* (*hash_func)(void*,size_t),size_t hash_func_block_size,size_t hash_func_digest_size,char* password, size_t password_len, char* data, size_t data_len){
-	void* HMAC_BLOCK(void* input,size_t input_len, size_t block_size, size_t digest_size,void* (*hasher)(void*,size_t)){
-		char* output = malloc(block_size);
-		if(input_len<block_size){
-			memcpy(output,input,input_len);
-			for(size_t i = input_len; i<block_size;i++){
-				output[i] = 0;
-			}
-			return output;
+    //Function for getting the HMAC Block(inside the HMAC function to prevent user usage)
+    void* HMAC_BLOCK(void* input,size_t input_len, size_t block_size, size_t digest_size,void* (*hasher)(void*,size_t)){
+	char* output = (*allocator)(block_size);
+
+	//zeros out the remaining values if the input isn't large enough
+	if(input_len<block_size){
+		memcpy(output,input,input_len);
+		for(size_t i = input_len; i<block_size;i++){
+			output[i] = 0;
 		}
-		if(input_len>block_size){
-			void* hash = hasher(input,input_len);
-			memcpy(output, hash, digest_size);
-			free(hash);
-			for(size_t i = digest_size;i<block_size;i++){
-				output[i] = 0;
-			}
-			return output;
+		return output;
+	}
+	//if the input is too large we hash and zero it till it reaches the block size
+	if(input_len>block_size){
+		void* hash = hasher(input,input_len);
+		memcpy(output, hash, digest_size);
+		(*intern_free)(hash);
+		for(size_t i = digest_size;i<block_size;i++){
+			output[i] = 0;
 		}
-		if(input_len==block_size){
-			memcpy(output,input,input_len);
-			return output;
-		}
-		//if you get back a NULL from this function it's unintended behavior and something fucky is going on
-		return NULL;
+		return output;
 	}
-	char* block = HMAC_BLOCK(password,password_len,hash_func_block_size,hash_func_digest_size,hash_func);
-	char* opad_block = malloc(hash_func_block_size);
-	for(size_t i = 0;i<hash_func_block_size;i++){
-		opad_block[i] = 0x5c^block[i];
+	//if the input is the blocksize we can just return the input
+	if(input_len==block_size){
+		memcpy(output,input,input_len);
+		return output;
 	}
-	char* ipad_block = malloc(hash_func_block_size+data_len);
-	for(size_t i = 0;i<hash_func_block_size;i++){
-		ipad_block[i] = 0x36^block[i];
-	}
-	memcpy((ipad_block+hash_func_block_size),data,data_len);
-	void* ipad_output = (*hash_func)(ipad_block,hash_func_block_size+data_len);
-	free(ipad_block);
-	void* hash_input = malloc(hash_func_block_size+hash_func_digest_size);
-	memcpy(hash_input,opad_block,hash_func_block_size);
-	memcpy((hash_input+hash_func_block_size),ipad_output,hash_func_digest_size);
-	char* output = (*hash_func)(hash_input,hash_func_block_size+hash_func_digest_size);
-	free(hash_input);
-	free(opad_block);
-	free(ipad_output);
-	return output;
+	//if you get back a NULL from this function it's unintended behavior and something fucky is going on
+	return NULL;
+    }
+
+    //getting our HMAC block
+    char* block = HMAC_BLOCK(password,password_len,hash_func_block_size,hash_func_digest_size,hash_func);
+    //error checking
+    if(block == NULL) return NULL;
+    
+    //generating the opadding
+    char* opad_block = (*allocator)(hash_func_block_size);
+    for(size_t i = 0;i<hash_func_block_size;i++){
+	opad_block[i] = 0x5c^block[i];
+    }
+    //generating the ipadding
+    char* ipad_block = (*allocator)(hash_func_block_size+data_len);
+    for(size_t i = 0;i<hash_func_block_size;i++){
+    	ipad_block[i] = 0x36^block[i];
+    }
+    //getting the final product with ipadding and hashing as an intermediate product towards the final HMAC
+    memcpy((ipad_block+hash_func_block_size),data,data_len);
+    void* ipad_output = (*hash_func)(ipad_block,hash_func_block_size+data_len);
+    (*intern_free)(ipad_block);
+    
+    //gathering the hash input all toegether
+    void* hash_input = (*allocator)(hash_func_block_size+hash_func_digest_size);
+    memcpy(hash_input,opad_block,hash_func_block_size);
+    memcpy((hash_input+hash_func_block_size),ipad_output,hash_func_digest_size);
+    
+    //taking the final hash for our output
+    char* output = (*hash_func)(hash_input,hash_func_block_size+hash_func_digest_size);
+
+    //freeing all the memory we used and returning the output
+    (*intern_free)(hash_input);
+    (*intern_free)(opad_block);
+    (*intern_free)(ipad_output);
+    //IF YOU GET SOME WEIRD MEMORY GLITCH USING THIS FUNCTION LOOK HERE I DON'T REMEMBER IF I DEALLOCATED THIS SOMEWHERE ELSE BUT I THINK I DIDN'T
+    (*intern_free)(block);
+    return output;
 }
 //takes in binary data with it's length and spits out the hex
 char* hexify(unsigned char* bin_data,size_t data_len){
 	char lookup[] = "0123456789ABCDEF";
-	char* output = malloc(data_len*2);
+	char* output = (*allocator)(data_len*2);
 	for(size_t i = 0;i<data_len;i++){
 		char first = bin_data[i]/16%16;
 		char second = bin_data[i]%16;
