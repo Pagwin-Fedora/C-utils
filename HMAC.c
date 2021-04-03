2c859e3bd92127c283ac1fba9b4303f9bb121cd0
Pagwin <git@pagwin.xyz>
2021-04-03 15:38:16 -0400

    added some notes

--- HMAC.c
+++ HMAC.c
@@ -6,37 +6,38 @@ void setAlloc(void* (*alloc)(size_t)){
 }
 void setDealloc(void (*intern_free)(void*) dealloc){
     intern_free = dealloc;
-}
+}t
+//HMAC takes the hash function, the hash function's block and digest sizes(I forget what those mean but they're needed to calculate things) the password to HMAC with the hash function the length of the password and some data to be put with the password and then spits out a
 char* HMAC(void* (*hash_func)(void*,size_t),size_t hash_func_block_size,size_t hash_func_digest_size,char* password, size_t password_len, char* data, size_t data_len){
-    //Function for getting the HMAC Block(inside the HMAC function to prevent user usage)
+    //Function for getting the HMAC Block(inside the HMAC function to prevent user usage) Edit: past me doesn't know what header files are for, the reason it's inside this function is to make it clear it's only used here
     void* HMAC_BLOCK(void* input,size_t input_len, size_t block_size, size_t digest_size,void* (*hasher)(void*,size_t)){
-	char* output = (*allocator)(block_size);
+			char* output = (*allocator)(block_size);
 
-	//zeros out the remaining values if the input isn't large enough
-	if(input_len<block_size){
-		memcpy(output,input,input_len);
-		for(size_t i = input_len; i<block_size;i++){
-			output[i] = 0;
-		}
-		return output;
-	}
-	//if the input is too large we hash and zero it till it reaches the block size
-	if(input_len>block_size){
-		void* hash = hasher(input,input_len);
-		memcpy(output, hash, digest_size);
-		(*intern_free)(hash);
-		for(size_t i = digest_size;i<block_size;i++){
-			output[i] = 0;
-		}
-		return output;
-	}
-	//if the input is the blocksize we can just return the input
-	if(input_len==block_size){
-		memcpy(output,input,input_len);
-		return output;
-	}
-	//if you get back a NULL from this function it's unintended behavior and something fucky is going on
-	return NULL;
+			//zeros out the remaining values if the input isn't large enough
+			if(input_len<block_size){
+				memcpy(output,input,input_len);
+				for(size_t i = input_len; i<block_size;i++){
+					output[i] = 0;
+				}
+				return output;
+			}
+			//if the input is too large we hash and zero it till it reaches the block size
+			if(input_len>block_size){
+				void* hash = hasher(input,input_len);
+				memcpy(output, hash, digest_size);
+				(*intern_free)(hash);
+				for(size_t i = digest_size;i<block_size;i++){
+					output[i] = 0;
+				}
+				return output;
+			}
+			//if the input is the blocksize we can just return the input
+			if(input_len==block_size){
+				memcpy(output,input,input_len);
+				return output;
+			}
+			//if you get back a NULL from this function it's unintended behavior and something fucky is going on
+			return NULL;
     }
 
     //getting our HMAC block
@@ -64,7 +65,7 @@ char* HMAC(void* (*hash_func)(void*,size_t),size_t hash_func_block_size,size_t h
     memcpy(hash_input,opad_block,hash_func_block_size);
     memcpy((hash_input+hash_func_block_size),ipad_output,hash_func_digest_size);
     
-    //taking the final hash for our output
+    //taking the final hash for our output Edit: I forget if output is actually a string or if I just didn't have a better data type for this but given I don't hexify it I'm gonna guess it's binary output
     char* output = (*hash_func)(hash_input,hash_func_block_size+hash_func_digest_size);
 
     //freeing all the memory we used and returning the output
@@ -77,6 +78,7 @@ char* HMAC(void* (*hash_func)(void*,size_t),size_t hash_func_block_size,size_t h
 }
 //takes in binary data with it's length and spits out the hex
 char* hexify(unsigned char* bin_data,size_t data_len){
+	//yes there is a way to make this lookup table unnecessary but no you shouldn't do it
 	char lookup[] = "0123456789ABCDEF";
 	char* output = (*allocator)(data_len*2);
 	for(size_t i = 0;i<data_len;i++){
