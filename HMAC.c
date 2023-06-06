be8b51e3ccf7ffe80137ea2c0a5719956bb7ea38
Pagwin <git@pagwin.xyz>
2023-06-05 20:12:55 -0400

    moved HMAC block and changed indentation a bit

--- HMAC.c
+++ HMAC.c
@@ -1,39 +1,11 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+
+inline void* HMAC_BLOCK(void* input,size_t input_len, size_t block_size, size_t digest_size,void* (*hasher)(void*,size_t));
 //this file is a mess and can 100% be improved on for readability
 //HMAC takes the hash function, the hash function's block and digest sizes(I forget what those mean but they're needed to calculate things) the password to HMAC with the hash function the length of the password and some data to be put with the password and then spits out a
 char* HMAC(void* (*hash_func)(void*,size_t),size_t hash_func_block_size,size_t hash_func_digest_size,char* password, size_t password_len, char* data, size_t data_len){
-    //Function for getting the HMAC Block(inside the HMAC function to prevent user usage) Edit: past me doesn't know what header files are for, the reason it's inside this function is to make it clear it's only used here
-    inline void* HMAC_BLOCK(void* input,size_t input_len, size_t block_size, size_t digest_size,void* (*hasher)(void*,size_t)){
-			char* output = malloc(block_size);
-
-			//zeros out the remaining values if the input isn't large enough
-			if(input_len<block_size){
-				memcpy(output,input,input_len);
-				for(size_t i = input_len; i<block_size;i++){
-					output[i] = 0;
-				}
-				return output;
-			}
-			//if the input is too large we hash and zero it till it reaches the block size
-			if(input_len>block_size){
-				void* hash = hasher(input,input_len);
-				memcpy(output, hash, digest_size);
-				free(hash);
-				for(size_t i = digest_size;i<block_size;i++){
-					output[i] = 0;
-				}
-				return output;
-			}
-			//if the input is the blocksize we can just return the input
-			if(input_len==block_size){
-				memcpy(output,input,input_len);
-				return output;
-			}
-			//if you get back a NULL from this function it's unintended behavior and something fucky is going on
-			return NULL;
-    }
 
     //getting our HMAC block
     char* block = HMAC_BLOCK(password,password_len,hash_func_block_size,hash_func_digest_size,hash_func);
@@ -70,6 +42,36 @@ char* HMAC(void* (*hash_func)(void*,size_t),size_t hash_func_block_size,size_t h
     //IF YOU GET SOME WEIRD MEMORY GLITCH USING THIS FUNCTION LOOK HERE I DON'T REMEMBER IF I DEALLOCATED THIS SOMEWHERE ELSE BUT I THINK I DIDN'T
     free(block);
     return output;
+}
+//Function for getting the HMAC Block(was inside the HMAC function to prevent user usage but that seems to that seems to not be fully portable so I moved it out) Edit: past me doesn't know what header files are for, the reason it's inside this function is to make it clear it's only used here
+inline void* HMAC_BLOCK(void* input,size_t input_len, size_t block_size, size_t digest_size,void* (*hasher)(void*,size_t)){
+    char* output = malloc(block_size);
+
+    //zeros out the remaining values if the input isn't large enough
+    if(input_len<block_size){
+	    memcpy(output,input,input_len);
+	    for(size_t i = input_len; i<block_size;i++){
+		    output[i] = 0;
+	    }
+	    return output;
+    }
+    //if the input is too large we hash and zero it till it reaches the block size
+    if(input_len>block_size){
+	    void* hash = hasher(input,input_len);
+	    memcpy(output, hash, digest_size);
+	    free(hash);
+	    for(size_t i = digest_size;i<block_size;i++){
+		    output[i] = 0;
+	    }
+	    return output;
+    }
+    //if the input is the blocksize we can just return the input
+    if(input_len==block_size){
+	    memcpy(output,input,input_len);
+	    return output;
+    }
+    //if you get back a NULL from this function it's unintended behavior and something fucky is going on
+    return NULL;
 }
 //takes in binary data with it's length and spits out the hex
 char* hexify(unsigned char* bin_data,size_t data_len){
